f80f6915350527dc17e1c953265f8e48d48b543c
Vladimir Yartsev <vovayartsev@gmail.com>
2015-03-12 01:12:35 +0400

    Testing version

---
+++ test/test_version.rb
@@ -0,0 +1,18 @@
+require 'open-uri'
+require 'wkhtmltopdf_installer'
+
+OFFICIAL_DOWNLOADS_PAGE = 'http://wkhtmltopdf.org/downloads.html'
+
+def up_to_date?
+  content = open(OFFICIAL_DOWNLOADS_PAGE) { |f| f.read }
+  if content =~ %r{The current stable version is <strong>([\d\.]+)</strong>}
+    $1 == WkhtmltopdfInstaller::VERSION
+  else
+    raise "Could not deduce the current stable version from #{OFFICIAL_DOWNLOADS_PAGE}"
+  end
+end
+
+unless up_to_date?
+  puts "Looks like update issued!"
+  exit 1
+end
