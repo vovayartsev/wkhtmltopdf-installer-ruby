865a0a84caecbeee41cbca907171c3286f65aad3
Vladimir Yartsev <vovayartsev@gmail.com>
2016-02-07 21:57:07 +0400

    Fixed tests

--- test/test_version.rb
+++ test/test_version.rb
@@ -5,7 +5,7 @@ OFFICIAL_DOWNLOADS_PAGE = 'http://wkhtmltopdf.org/downloads.html'
 
 def up_to_date?
   content = open(OFFICIAL_DOWNLOADS_PAGE) { |f| f.read }
-  if content =~ %r{The current stable version is <strong>([\d\.]+)</strong>}
+  if content =~ %r{The current stable series is <strong>([\d\.]+)</strong>}
     $1 == WkhtmltopdfInstaller::VERSION
   else
     raise "Could not deduce the current stable version from #{OFFICIAL_DOWNLOADS_PAGE}"
